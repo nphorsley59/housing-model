5e9e2fc6dc3c47013988113debb4da29f4834cc4
nphorsley59 <nphorsley59@gmail.com>
2020-05-19 21:25:09 -0500

    setup for data cleaning

---
+++ data_cleaning.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+df = pd.read_csv('train.csv')
+
+# summarize data
+# deal with NAs
+# ... continue 5/20
